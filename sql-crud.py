d518a0a0c3d204f01f6d14e1332d31044007eef8
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-02-12 11:59:32 +0000

    Add more programmers to Programmers table

--- sql-crud.py
+++ sql-crud.py
@@ -40,8 +40,62 @@ ada_lovelace = Programmer(
     famous_for="First Programmer"
 )
 
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+simon_jayawardena = Programmer (
+    first_name="Simon",
+    last_name="Jayawardena",
+    gender="M",
+    nationality="British",
+    famous_for="Full Stack Developer"
+)
+
 # add each instance of our programmers to our session
-session.add(ada_lovelace)
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session. add(simon_jayawardena)
 
 #commit our session to the database
 session.commit()
